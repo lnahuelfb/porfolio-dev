c2d4ae89cacdf013f870881760f66d4821ab0cc4
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-03-17 16:22:51 -0300

    Agregada la compatibilidad con vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react"
+import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  adapter: vercel()
 });
